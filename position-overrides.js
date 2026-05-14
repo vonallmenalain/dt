9c968a5604e810e50a1a18b9423e16049a899ee4
vonallmenalain <alain.sc2@gmail.com>
2026-05-14 05:32:50 +0200

    Update position-overrides.js

--- position-overrides.js
+++ position-overrides.js
@@ -1,152 +1,149 @@
 // Positions-Overrides für DreamTeam
-// Ergänzt am: 7.5.2026
+// Generiert am: 14.5.2026, 05:31:59
 // Aktives Turnier: WM 2026 (wm2026)
-// Hinweis: wm2026 enthält MIDFIELDER-Spieler aus data-wm2026.js,
-// die realistisch als Stürmer, Flügelstürmer, Wide Forward oder offensiver Flügelspieler geführt werden können.
 
 window.POSITION_OVERRIDES = {
     wm2022: {},
 
     em2024: {
-        "1422": "ATTACKER", // Jérémy Doku
+        "1422": "ATTACKER", // Jeremy Doku
         "502": "MIDFIELDER" // Joshua Kimmich
     },
 
     wm2026: {
-        "17": "ATTACKER", // Christian Pulisic (USA)
-        "247": "ATTACKER", // Cody Gakpo (Netherlands)
-        "207": "ATTACKER", // Ivan Perišić (Croatia)
-        "1422": "ATTACKER", // Jérémy Doku (Belgium)
-        "978": "ATTACKER", // Kai Havertz (Germany)
-        "386828": "ATTACKER", // Lamine Yamal (Spain)
-        "153": "ATTACKER", // Masour Dembélé (France; API-Name, vermutlich Ousmane Dembélé)
-        "1496": "ATTACKER", // Raphinha (Brazil)
-        "10009": "ATTACKER", // Rodrygo (Brazil)
-        "510": "ATTACKER", // Serge Gnabry (Germany)
-        "762": "ATTACKER", // Vinícius Júnior (Brazil)
-        "329163": "ATTACKER", // Adil Boulbina (Algeria)
-        "276670": "ATTACKER", // Farès Chaïbi (Algeria)
-        "19071": "ATTACKER", // Emiliano Buendía (Argentina)
-        "449249": "ATTACKER", // Franco Mastantuono (Argentina)
-        "6067": "ATTACKER", // Thiago Almada (Argentina)
-        "44843": "ATTACKER", // Martin Boyle (Australia)
-        "126642": "ATTACKER", // Patrick Wimmer (Austria)
-        "327895": "ATTACKER", // Paul Wanner (Austria)
-        "1417": "ATTACKER", // Alexis Saelemaekers (Belgium)
-        "265785": "ATTACKER", // Luiz Henrique (Brazil)
-        "193279": "ATTACKER", // Jayden Nelson (Canada)
-        "284061": "ATTACKER", // Marcelo Flores (Canada)
-        "51016": "ATTACKER", // Tajon Buchanan (Canada)
-        "265556": "ATTACKER", // Telmo Arcanjo (Cape Verde Islands)
-        "345748": "ATTACKER", // Carlos Gómez (Colombia)
-        "13376": "ATTACKER", // Jaminton Campaz (Colombia)
-        "13708": "ATTACKER", // Jhon Arias (Colombia)
-        "281795": "ATTACKER", // Yáser Asprilla (Colombia)
-        "3034": "ATTACKER", // Meschack Elia (Congo DR)
-        "129670": "ATTACKER", // Nathanaël Mbuku (Congo DR)
-        "8627": "ATTACKER", // Théo Bongonda (Congo DR)
-        "260865": "ATTACKER", // Marco Pašalić (Croatia)
-        "37272": "ATTACKER", // Brandley Kuwas (Curaçao)
-        "906": "ATTACKER", // Tahith Chong (Curaçao)
-        "290212": "ATTACKER", // Denis Višinský (Czech Republic)
-        "66253": "ATTACKER", // Tomáš Ladra (Czech Republic)
-        "25414": "ATTACKER", // John Yeboah (Ecuador)
-        "311543": "ATTACKER", // Nilson Angulo (Ecuador)
-        "406303": "ATTACKER", // Ray Páez (Ecuador)
-        "306940": "ATTACKER", // Yaimar Medina (Ecuador)
-        "70535": "ATTACKER", // Ibrahim Adel (Egypt)
-        "16901": "ATTACKER", // Islam Issa (Egypt)
-        "16871": "ATTACKER", // Zizo (Egypt)
-        "136723": "ATTACKER", // Chukwunonso Madueke (England)
-        "152982": "ATTACKER", // Cole Palmer (England)
-        "19586": "ATTACKER", // Eberechi Eze (England)
-        "18778": "ATTACKER", // Harvey Barnes (England)
-        "631": "ATTACKER", // Philip Foden (England)
-        "174565": "ATTACKER", // Hugo Ekitike (France)
-        "274300": "ATTACKER", // Maghnes Akliouche (France)
-        "156477": "ATTACKER", // Mathis Cherki (France)
-        "19617": "ATTACKER", // Michael Olise (France)
-        "24798": "ATTACKER", // Chris Führich (Germany)
-        "178077": "ATTACKER", // Kevin Schade (Germany)
-        "494131": "ATTACKER", // Lennart Karl (Germany)
-        "19281": "ATTACKER", // Antoine Semenyo (Ghana)
-        "3428": "ATTACKER", // Jordan Ayew (Ghana)
-        "1944": "ATTACKER", // Joseph Paintsil (Ghana)
-        "15911": "ATTACKER", // Mohammed Kudus (Ghana)
-        "526524": "ATTACKER", // Prince Owusu (Ghana)
-        "50958": "ATTACKER", // Derrick Etienne (Haiti)
-        "174915": "ATTACKER", // Josué Casimir (Haiti)
-        "8564": "ATTACKER", // Ali Gholizadeh (Iran)
-        "29937": "ATTACKER", // Amirhossein Hosseinzadeh (Iran)
-        "290554": "ATTACKER", // Mohammad Mohebi (Iran)
-        "134217": "ATTACKER", // Mohammad Mohebi (Iran)
-        "348089": "ATTACKER", // Hasan Abdulkareem (Iraq)
-        "140747": "ATTACKER", // Ibraheem Bayesh (Iraq)
-        "3247": "ATTACKER", // Dazet Zaha (Ivory Coast)
-        "1942": "ATTACKER", // Junya Ito (Japan)
-        "106835": "ATTACKER", // Kaoru Mitoma (Japan)
-        "2598": "ATTACKER", // Ritsu Dōan (Japan)
-        "32862": "ATTACKER", // Takefusa Kubo (Japan)
-        "1101": "ATTACKER", // Takumi Minamino (Japan)
-        "199143": "ATTACKER", // Yuito Suzuki (Japan)
-        "123530": "ATTACKER", // Mahmoud Al Mardi (Jordan)
-        "72142": "ATTACKER", // Mohammed Abu Zurayq (Jordan)
-        "1577": "ATTACKER", // Diego Lainez (Mexico)
-        "51068": "ATTACKER", // Efraín Álvarez (Mexico)
-        "482605": "ATTACKER", // Gilberto Mora (Mexico)
-        "181421": "ATTACKER", // Abdessamad Ezzalzouli (Morocco)
-        "744": "ATTACKER", // Brahim Díaz (Morocco)
-        "162016": "ATTACKER", // Xavier Simons (Netherlands)
-        "314266": "ATTACKER", // Luciano Valente (Netherlands)
-        "94322": "ATTACKER", // Callum McCowatt (New Zealand)
-        "6935": "ATTACKER", // Sarpreet Singh (New Zealand)
-        "314511": "ATTACKER", // Antonio Nusa (Norway)
-        "39115": "ATTACKER", // Aron Dønnum (Norway)
-        "39073": "ATTACKER", // Jens Hauge (Norway)
-        "278133": "ATTACKER", // Oscar Bobb (Norway)
-        "57875": "ATTACKER", // César Yanis (Panama)
-        "2982": "ATTACKER", // Omar Browne (Panama)
-        "2514": "ATTACKER", // Alejandro Romero (Paraguay)
-        "278370": "ATTACKER", // Diego Gómez (Paraguay)
-        "70747": "ATTACKER", // Julio Enciso (Paraguay)
-        "636": "ATTACKER", // Bernardo Silva (Portugal)
-        "1864": "ATTACKER", // Pedro Neto (Portugal)
-        "18748": "ATTACKER", // Pote (Portugal)
-        "41112": "ATTACKER", // Trincão (Portugal)
-        "147812": "ATTACKER", // Ayman Yahya (Saudi Arabia)
-        "381176": "ATTACKER", // Marwan Al Sahafi (Saudi Arabia)
-        "44340": "ATTACKER", // Salem Al Dawsari (Saudi Arabia)
-        "44374": "ATTACKER", // Turki Al Ammar (Saudi Arabia)
-        "1125": "ATTACKER", // Ryan Christie (Scotland)
-        "282767": "ATTACKER", // Tommy Conway (Scotland)
-        "483109": "ATTACKER", // Mohau Nkota (South Africa)
-        "179893": "ATTACKER", // Oswin Appollis (South Africa)
-        "295977": "ATTACKER", // Tshepang Moremi (South Africa)
-        "357286": "ATTACKER", // Bae Jun-Ho (South Korea)
-        "24888": "ATTACKER", // Hwang Hee-Chan (South Korea)
-        "927": "ATTACKER", // Lee Kang-In (South Korea)
-        "423708": "ATTACKER", // Yang Min-Hyeok (South Korea)
-        "47317": "ATTACKER", // Barrenetxea (Spain)
-        "184226": "ATTACKER", // Yeremy Pino (Spain)
-        "182219": "ATTACKER", // Álex Baena (Spain)
-        "350850": "ATTACKER", // Besfort Zeneli (Sweden)
-        "160925": "ATTACKER", // Taha Ali (Sweden)
-        "406244": "ATTACKER", // Johan Manzambi (Switzerland)
-        "49469": "ATTACKER", // Ahmed Ben Ouanes (Tunisia)
-        "42012": "ATTACKER", // Mohamed Achouri (Tunisia)
-        "291964": "ATTACKER", // Arda Güler (Türkiye)
-        "161790": "ATTACKER", // Doğuhan Şimşir (Türkiye)
-        "134590": "ATTACKER", // Oğuz Aydın (Türkiye)
-        "51620": "ATTACKER", // Facundo Torres (Uruguay)
-        "51553": "ATTACKER", // Federico Martínez (Uruguay)
-        "51464": "ATTACKER", // Ignacio Laquintana (Uruguay)
-        "51618": "ATTACKER", // Paul Rodríguez (Uruguay)
-        "16482": "ATTACKER", // Rodrigo Aguirre (Uruguay)
-        "50739": "ATTACKER", // Brenden Aaronson (USA)
-        "312896": "ATTACKER", // Diego Luna (USA)
-        "161921": "ATTACKER", // Giovanni Reyna (USA)
-        "162037": "ATTACKER", // Malik Tillman (USA)
-        "53834": "ATTACKER" // Dostonbek Khamdamov (Uzbekistan)
+        "181421": "ATTACKER", // Abdessamad Ezzalzouli
+        "329163": "ATTACKER", // Adil Boulbina
+        "49469": "ATTACKER", // Ahmed Ben Ouanes
+        "2514": "ATTACKER", // Alejandro Romero
+        "182219": "ATTACKER", // Alex Baena
+        "1417": "ATTACKER", // Alexis Saelemaekers
+        "8564": "ATTACKER", // Ali Gholizadeh
+        "29937": "ATTACKER", // Amirhossein Hosseinzadeh
+        "19281": "ATTACKER", // Antoine Semenyo
+        "314511": "ATTACKER", // Antonio Nusa
+        "39115": "ATTACKER", // Aron Donnum
+        "147812": "ATTACKER", // Ayman Yahya
+        "357286": "ATTACKER", // Bae Jun-Ho
+        "47317": "ATTACKER", // Barrenetxea
+        "636": "ATTACKER", // Bernardo Silva
+        "350850": "ATTACKER", // Besfort Zeneli
+        "744": "ATTACKER", // Brahim Diaz
+        "37272": "ATTACKER", // Brandley Kuwas
+        "50739": "ATTACKER", // Brenden Aaronson
+        "94322": "ATTACKER", // Callum McCowatt
+        "345748": "ATTACKER", // Carlos Gomez
+        "57875": "ATTACKER", // Cesar Yanis
+        "24798": "ATTACKER", // Chris Führich
+        "17": "ATTACKER", // Christian Pulisic
+        "136723": "ATTACKER", // Chukwunonso Madueke
+        "247": "ATTACKER", // Cody Gakpo
+        "152982": "ATTACKER", // Cole Palmer
+        "3247": "ATTACKER", // Dazet Zaha
+        "290212": "ATTACKER", // Denis Visinsky
+        "50958": "ATTACKER", // Derrick Etienne
+        "278370": "ATTACKER", // Diego Gomez
+        "1577": "ATTACKER", // Diego Lainez
+        "312896": "ATTACKER", // Diego Luna
+        "161790": "ATTACKER", // Doguhan Simsir
+        "53834": "ATTACKER", // Dostonbek Khamdamov
+        "19586": "ATTACKER", // Eberechi Eze
+        "51068": "ATTACKER", // Efrain Alvarez
+        "19071": "ATTACKER", // Emiliano Buendia
+        "51620": "ATTACKER", // Facundo Torres
+        "276670": "ATTACKER", // Fares Chaibi
+        "51553": "ATTACKER", // Federico Martinez
+        "449249": "ATTACKER", // Franco Mastantuono
+        "482605": "ATTACKER", // Gilberto Mora
+        "161921": "ATTACKER", // Giovanni Reyna
+        "18778": "ATTACKER", // Harvey Barnes
+        "348089": "ATTACKER", // Hasan Abdulkareem
+        "174565": "ATTACKER", // Hugo Ekitike
+        "24888": "ATTACKER", // Hwang Hee-Chan
+        "140747": "ATTACKER", // Ibraheem Bayesh
+        "70535": "ATTACKER", // Ibrahim Adel
+        "51464": "ATTACKER", // Ignacio Laquintana
+        "16901": "ATTACKER", // Islam Issa
+        "207": "ATTACKER", // Ivan Perisic
+        "13376": "ATTACKER", // Jaminton Campaz
+        "193279": "ATTACKER", // Jayden Nelson
+        "39073": "ATTACKER", // Jens Hauge
+        "1422": "ATTACKER", // Jeremy Doku
+        "13708": "ATTACKER", // Jhon Arias
+        "406244": "ATTACKER", // Johan Manzambi
+        "25414": "ATTACKER", // John Yeboah
+        "3428": "ATTACKER", // Jordan Ayew
+        "1944": "ATTACKER", // Joseph Paintsil
+        "174915": "ATTACKER", // Josue Casimir
+        "70747": "ATTACKER", // Julio Enciso
+        "1942": "ATTACKER", // Junya Ito
+        "978": "ATTACKER", // Kai Havertz
+        "106835": "ATTACKER", // Kaoru Mitoma
+        "178077": "ATTACKER", // Kevin Schade
+        "386828": "ATTACKER", // Lamine Yamal
+        "927": "ATTACKER", // Lee Kang-In
+        "494131": "ATTACKER", // Lennart Karl
+        "314266": "ATTACKER", // Luciano Valente
+        "265785": "ATTACKER", // Luiz Henrique
+        "274300": "ATTACKER", // Maghnes Akliouche
+        "123530": "ATTACKER", // Mahmoud Al Mardi
+        "162037": "ATTACKER", // Malik Tillman
+        "284061": "ATTACKER", // Marcelo Flores
+        "260865": "ATTACKER", // Marco Pasalic
+        "44843": "ATTACKER", // Martin Boyle
+        "381176": "ATTACKER", // Marwan Al Sahafi
+        "153": "ATTACKER", // Masour Dembele
+        "156477": "ATTACKER", // Mathis Cherki
+        "3034": "ATTACKER", // Meschack Elia
+        "19617": "ATTACKER", // Michael Olise
+        "42012": "ATTACKER", // Mohamed Achouri
+        "134217": "ATTACKER", // Mohammad Mohebi
+        "290554": "ATTACKER", // Mohammad Mohebi
+        "72142": "ATTACKER", // Mohammed Abu Zurayq
+        "15911": "ATTACKER", // Mohammed Kudus
+        "483109": "ATTACKER", // Mohau Nkota
+        "129670": "ATTACKER", // Nathanael Mbuku
+        "311543": "ATTACKER", // Nilson Angulo
+        "134590": "ATTACKER", // Oguz Aydin
+        "2982": "ATTACKER", // Omar Browne
+        "278133": "ATTACKER", // Oscar Bobb
+        "179893": "ATTACKER", // Oswin Appollis
+        "126642": "ATTACKER", // Patrick Wimmer
+        "51618": "ATTACKER", // Paul Rodriguez
+        "327895": "ATTACKER", // Paul Wanner
+        "1864": "ATTACKER", // Pedro Neto
+        "631": "ATTACKER", // Philip Foden
+        "18748": "ATTACKER", // Pote
+        "526524": "ATTACKER", // Prince Owusu
+        "1496": "ATTACKER", // Raphinha
+        "406303": "ATTACKER", // Ray Paez
+        "2598": "ATTACKER", // Ritsu Doan
+        "16482": "ATTACKER", // Rodrigo Aguirre
+        "10009": "ATTACKER", // Rodrygo
+        "1125": "ATTACKER", // Ryan Christie
+        "44340": "ATTACKER", // Salem Al Dawsari
+        "6935": "ATTACKER", // Sarpreet Singh
+        "510": "ATTACKER", // Serge Gnabry
+        "160925": "ATTACKER", // Taha Ali
+        "906": "ATTACKER", // Tahith Chong
+        "51016": "ATTACKER", // Tajon Buchanan
+        "32862": "ATTACKER", // Takefusa Kubo
+        "1101": "ATTACKER", // Takumi Minamino
+        "265556": "ATTACKER", // Telmo Arcanjo
+        "8627": "ATTACKER", // Theo Bongonda
+        "6067": "ATTACKER", // Thiago Almada
+        "66253": "ATTACKER", // Tomas Ladra
+        "282767": "ATTACKER", // Tommy Conway
+        "41112": "ATTACKER", // Trincao
+        "295977": "ATTACKER", // Tshepang Moremi
+        "44374": "ATTACKER", // Turki Al Ammar
+        "762": "ATTACKER", // Vinicius Junior
+        "162016": "ATTACKER", // Xavier Simons
+        "306940": "ATTACKER", // Yaimar Medina
+        "423708": "ATTACKER", // Yang Min-Hyeok
+        "281795": "ATTACKER", // Yaser Asprilla
+        "184226": "ATTACKER", // Yeremy Pino
+        "199143": "ATTACKER", // Yuito Suzuki
+        "16871": "ATTACKER" // Zizo
     },
 
     em2028: {},
